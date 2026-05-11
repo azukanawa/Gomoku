b2aabbef7fd67202d532fb5cd6846ae3365c3612
NaiDou1 <109854081+NaiDou1@users.noreply.github.com>
2026-05-11 23:05:06 +0800

    添加了棋盘渲染的的尝试，加载了棋盘图片，并在窗口中显示出来。

--- src/main.c
+++ src/main.c
@@ -12,13 +12,16 @@
 int main() {
   struct window_size window_size;
   InitWindowSize(&window_size, 800, 600);
-
   InitWindow(window_size.width, window_size.height, "五子棋游戏test");
+  Texture ChessBoard_13 = LoadTexture("../res/Picture/chess_board_13.jpg");
+  SetTargetFPS(60);  // 设置帧率为60帧每秒
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(WHITE);
-    /* code */
+    /* 在这里添加渲染代码 */
+    DrawTexture(ChessBoard_13, 0, 0, WHITE);
     EndDrawing();
+    // 在这里添加逻辑判断代码
   }
   CloseWindow();  // 窗口被关闭,卸载OpenGL上下文
   return 0;
