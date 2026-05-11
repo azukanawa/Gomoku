d8ae1b663542e5980127e4f9ad52060b436fd374
azukanawa <13430690708@163.com>
2026-05-11 21:14:09 +0800

    feat: 添加初始化棋盘函数

--- src/function.c
+++ src/function.c
@@ -1,4 +1,25 @@
 #include "function.h"
+
+#include "main.h"
 int MaxInt(int a, int b) { return a > b ? a : b; }
 
 int MinInt(int a, int b) { return a < b ? a : b; }
+
+void InitBoard(ChessBoard* board) {
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    for (int j = 0; j < BOARD_SIZE; j++) {
+      board->board[i][j] = EMPTY;
+    }
+  }
+  board->emptyCeils = BOARD_SIZE * BOARD_SIZE;
+
+  return;
+}
+
+Bool IsBoardFull(const ChessBoard* board) {
+  if (board->emptyCeils == 0) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
