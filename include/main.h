056f55de50a140f3d1f1ad605bbe426f36944604
azukanawa <13430690708@163.com>
2026-05-11 21:15:11 +0800

    feat: 增加bool类型结构体定义

--- include/main.h
+++ include/main.h
@@ -1,17 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define BOARD_SIZE 15
-
+#define BOARD_SIZE 15  // 棋盘尺寸
+typedef enum { FALSE, TRUE } Bool;
 typedef enum {
   EMPTY,     // 空
   PLAYER_1,  // 玩家一
   PLAYER_2,  // 玩家二
-} Piece;
+} Piece;     // 格子状态
 
 typedef struct {
   Piece board
       [BOARD_SIZE]
       [BOARD_SIZE];  // 棋盘，用于存储当前格子的状态（是空的，玩家一的还是玩家二的）
   int emptyCeils;  // 当前剩余空格数
-} ChessBoard;
+} ChessBoard;      // 棋盘结构体
 #endif
