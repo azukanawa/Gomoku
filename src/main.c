8ecfa0ef00f697c5aaece8d7ccff2959f7a12437
NaiDou1 <109854081+NaiDou1@users.noreply.github.com>
2026-05-11 21:42:41 +0800

    加入了创建窗口和窗口管理的内容

--- src/main.c
+++ src/main.c
@@ -1,13 +1,20 @@
 #include "main.h"
 
 #include <limits.h>
+#include <raylib.h>
+#include <raymath.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-
-    while (1)
+    InitWindow(800, 600, "raylib [core] example - basic window");
+    while (!WindowShouldClose())
     {
+        BeginDrawing();
+        // ClearBackground();
         /* code */
+        EndDrawing();
     }
+    return 0;
 }
