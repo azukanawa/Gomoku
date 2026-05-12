832267c05d76166f8b4378670a2ad76797ce6de8
NaiDou1 <109854081+NaiDou1@users.noreply.github.com>
2026-05-12 20:43:18 +0800

    更改了main.c的结结构

--- src/main.c
+++ src/main.c
@@ -10,18 +10,20 @@
 #include "function.h"
 
 int main() {
+  // 设置渲染的基本参数
   struct window_size window_size;
+  SetTargetFPS(60);  // 设置帧率为60帧每秒
   InitWindowSize(&window_size, 800, 600);
+  // 初始化窗口,加载材质
   InitWindow(window_size.width, window_size.height, "五子棋游戏test");
   Texture ChessBoard_13 = LoadTexture("../res/Picture/chess_board_13.jpg");
-  SetTargetFPS(60);  // 设置帧率为60帧每秒
-  while (!WindowShouldClose()) {
+
+  while (!WindowShouldClose()) {  // 主循环从此处开始
     BeginDrawing();
     ClearBackground(WHITE);
     /* 在这里添加渲染代码 */
     DrawTexture(ChessBoard_13, 0, 0, WHITE);
     EndDrawing();
-    // 在这里添加逻辑判断代码
   }
   CloseWindow();  // 窗口被关闭,卸载OpenGL上下文
   return 0;
