da389a6b629a8de2248421d6641efe2b605cca5b
azukanawa <13430690708@163.com>
2026-05-15 23:47:17 +0800

    docs(main.h): 修改胜利的得分

--- include/main.h
+++ include/main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <limits.h>
 // 棋型评分常量
-#define SCORE_WIN INT_MAX  // 五子连珠
-#define SCORE_LIVE4 5000   // 活四
-#define SCORE_BLOCK4 2500  // 冲四（死四、跳冲四）
-#define SCORE_LIVE3 1000   // 活三（包括跳活三）
-#define SCORE_BLOCK3 200   // 眠三（只有一端开放的三）
-#define SCORE_LIVE2 100    // 活二（包括跳活二）
-#define SCORE_BLOCK2 30    // 眠二（只有一端开放的二）
+#define SCORE_WIN 10000000  // 五子连珠
+#define SCORE_LIVE4 5000    // 活四
+#define SCORE_BLOCK4 2500   // 冲四（死四、跳冲四）
+#define SCORE_LIVE3 1000    // 活三（包括跳活三）
+#define SCORE_BLOCK3 200    // 眠三（只有一端开放的三）
+#define SCORE_LIVE2 100     // 活二（包括跳活二）
+#define SCORE_BLOCK2 30     // 眠二（只有一端开放的二）
 
 typedef enum { FALSE, TRUE } Bool;
 typedef enum {
